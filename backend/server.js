a37680e72bf3fa987e482eea62cf1882850b9122
Sukta Chandra <suktachandra20@gmail.com>
2026-08-25 22:38:21 +0600

    Fix dns and mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first'); // Render IPV6 DNS Issue Fix
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -12,13 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// MongoDB Connection with IPv4 Fix
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://suktachandra20_db_user:Krishna1234@cluster0.y852dg8.mongodb.net/linkdrop?retryWrites=true&w=majority";
 
-
-const MONGO_URI = process.env.MONGO_URI || "mongodb://suktachandra20_db_user:Krishna1234@cluster0-shard-00-00.y852dg8.mongodb.net:27017,cluster0-shard-00-01.y852dg8.mongodb.net:27017,cluster0-shard-00-02.y852dg8.mongodb.net:27017/linkdrop?ssl=true&replicaSet=atlas-139bhn-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, { family: 4 })
   .then(() => console.log('MongoDB Connected Successfully!'))
   .catch((err) => console.log('Database Connection Error:', err));
+
 // Routes
 app.use('/api/auth', authRoutes);
 
